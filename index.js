09039ef7d487973ae4640b5df214ce289d34c48b
lfabrizio <louisfabrizio.student@careerdevs.com>
2019-10-05 14:24:16 -0400

    through 44

--- index.js
+++ index.js
@@ -56,5 +56,38 @@ good = true;
 for (var i = 0; i < 20; i++){
     order.push(Math.floor(Math.random() *4) + 1);
 }
-console.log(order);
+compTurn = true;
+
+intervalId = setInterval(gameTurn, 800);
+}
+
+function gameTurn() {
+    on = false;
+
+    if(flash = turn){
+        clearInterval(interval);
+        compTurn = false;
+        clearColor;
+        on = true;
+    }
+
+    if (compTurn) {
+        clearCorol();
+        setTimeout(() => {
+            if(order[flash] == 1) one();
+            if(order[flash] == 2) two();
+            if(order[flash] == 3) three();
+            if(order[flash] == 4) four();
+            flash++;
+        }, 200);
+    }
+}
+
+function one() {
+    if(noise) {
+        let audio = document.getElementById("clip1");
+        audio.play();
+    }
+    noise = true;
+    topLeft.getElementsByClassName.backgroundColor = "lightgreen";
 }
